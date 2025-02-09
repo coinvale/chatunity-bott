fc1c70d751d1c5c64d6cc745662ff26e3918f9c5
chatunity <appleidnewus@gmail.com>
2025-02-09 17:51:31 +0000

    update

--- plugins/info-installa.js
+++ plugins/info-installa.js
@@ -20,18 +20,18 @@ let neww = performance.now()
 let speed = (neww - old).toFixed(4)
 let prova = { "key": {"participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo"
 }, "message": { 
-"orderMessage": { text: '𝐁𝐢𝐱𝐛𝐲 𝐕𝐢𝐬𝐢𝐨𝐧 💬',
+"orderMessage": { text: '𝐒𝐂𝐀𝐑𝐈𝐂𝐀 𝐂𝐇𝐀𝐓𝐔𝐍𝐈𝐓𝐘-𝐁𝐎𝐓 💬',
 "itemCount": 2023,
 "status": 1,
 "surface" : 1,
-    "message": '𝐒𝐜𝐚𝐫𝐢𝐜𝐚 𝐁𝐢𝐱𝐛𝐲 𝐕𝐢𝐬𝐢𝐨𝐧 💬',
+    "message": '𝐒𝐂𝐀𝐑𝐈𝐂𝐀 𝐂𝐇𝐀𝐓𝐔𝐍𝐈𝐓𝐘-𝐁𝐎𝐓 💬',
 "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=15395490858:+1 (539) 549-0858\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD`
 }}, "participant": "0@s.whatsapp.net"
 }
 let info = `
 ═════ •⊰✦⊱• ═════
 𝐒𝐂𝐀𝐑𝐈𝐂𝐀 𝐂𝐇𝐀𝐓𝐔𝐍𝐈𝐓𝐘-𝐁𝐎𝐓
-https://github.com/chatunitybot/chatunity-bot
+https://github.com/chatunityy/chatunity-bot
 ═════ •⊰✦⊱• ═════
 `.trim() 
 conn.reply(m.chat, info,prova, m, {
